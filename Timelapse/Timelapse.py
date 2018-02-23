314f5ffec7d4d9421a38e10db86f87cebde601b7
kroissharp <mikx0067@edu.easj.dk>
2018-02-23 11:25:50 +0100

    Timelapse commit

--- Timelapse/Timelapse.py
+++ Timelapse/Timelapse.py
@@ -1,1 +1,33 @@
-
+#we need to use time for sleep & our file naming convention
+#pyimgur is a libary for imgur - we use this to upload our
+#picture and return a link
+import time
+from picamera import PiCamera
+import pyimgur
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+NameTime = (time.strftime(" %d-%m-%y_%H:%M"))
+camera.vflip = True #flip verti
+FileName = NameTime + ".jpg"
+   
+#this is our funktion:
+#takes a picture, upload it and save it on the device
+#framecount is used to organize the filenames
+def TimeLapse ():
+    framecount = 1
+    CLIENT_ID = "fc65e40de16806a"
+    im = pyimgur.Imgur(CLIENT_ID)
+    while True:            
+            strframecount = str(framecount)
+            PATH= strframecount + FileName
+            camera.capture(PATH)
+            framecount = framecount + 1
+            print("TimeLapse picture taken:" + NameTime)#tell user pic taken
+            #3200 = 1 time            
+            uploaded_image = im.upload_image(PATH, title="Uploaded with PyImgur")
+            print(uploaded_image.link)
+            time.sleep(3)
+            
+        
+if __name__ == "__main__":
+    TimeLapse()
